cf5820ed120dc0864af9a93535ff9fc3c06c25fe
aria khayer <ariakh110@gmail.com>
2022-05-14 23:51:36 +0430

    refine url

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import axios from 'axios';
 
 //call api
-const BASE_URL = 'https://www.metaweather.com/api';
-const server = axios.create({ baseURL: BASE_URL });
+const BASE_URL = 'https://www.metaweather.com/api/location';
+const CROSS_DOMAIN = 'https://reactweather0707.herokuapp.com';
+const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`;
 
 const useForecast = () => {
     const [isError, setError] = useState(false);
@@ -12,16 +13,13 @@ const useForecast = () => {
 
     //call the api
     const submitRequest = async location => {
+        console.log(location);
         //1.get woeid
-        const response = await server
-            .get('location/search', {
-                params: { query: location },
-            })
-            .then(response => {
-                console.log(response.data);
-            })
-            .catch(error => console.log(error));
+        const response = await axios(`${REQUEST_URL}/search`, {
+            params: { query: location },
+        });
         //2. get weather
+        console.log(response);
     };
 
     return {
